d031cfb9bd0414d428c4fbe6bd576d6d374f8423
Skomegna <48660280+Skomegna@users.noreply.github.com>
2024-03-17 18:29:42 -0700

    Update LongestSubstring_Palindrome.cpp

--- LongestSubstring_Palindrome.cpp
+++ LongestSubstring_Palindrome.cpp
@@ -1,10 +1,18 @@
+*/
+Time Complexity: O(N^2)    
+/*
+    
 class Solution {
 public:
 
     string expand(const string &s, int left, int right)
     {
         const size_t n = s.length();
-        while(left >= 0 && right < n && s[left] == s[right]) left--, right ++;
+        while(left >= 0 && right < n && s[left] == s[right])
+        {
+            left--;
+            right ++;
+        }
         return s.substr(left + 1, right - left - 1);
     }
 
@@ -16,9 +24,9 @@ public:
         for (int i = 0; i < n - 1; ++i)
         {
             temp = expand(s, i, i);
-            if (temp.length() > result.length()) result = temp;
+            if (temp.length() > result.length()) result = temp; //Odd case
             temp = expand(s, i, i + 1);
-            if (temp.length() > result.length()) result = temp;
+            if (temp.length() > result.length()) result = temp;    //Even case
         }
         return result;
     }
